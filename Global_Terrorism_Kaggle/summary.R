33aca5b8dfcfebaa7f237d351da65427c22e4dc9
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2019-11-26 08:22:57 -0300

    Update summary.R

--- Global_Terrorism_Kaggle/summary.R
+++ Global_Terrorism_Kaggle/summary.R
@@ -22,9 +22,10 @@ library(ggthemes)
 ggplot(sumkill,aes(x=reorder(sumkill$region,-sumkill$`sum(nkill, na.rm = T)`),y=sumkill$`sum(nkill, na.rm = T)`,fill=region))+geom_col()+theme_fivethirtyeight()+scale_fill_ptol()+theme(legend.position = "none")+
   labs(y="Frequency",title = "Number of kills per region from 1970 to 2017",x="Regions")+geom_text(aes(label=sumkill$`sum(nkill, na.rm = T)`),vjust=-0.1) +coord_flip() 
 
-killregion <- as.data.frame(sumkill)
-#n of kills per region
 
+#n of kills per region
+killregion <- as.data.frame(sumkill)
+population <- c()#source:https://www.worldometers.info/
 
 #terrorist groups in iraq
 iraqt <- terrorism %>% filter(country_txt=="Iraq")
